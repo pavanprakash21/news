4c5821841bbbacc68d679c2a56854b4782d632d0
pavan <pavan@morethanmetrics.com>
2021-09-08 21:24:19 +0200

    ignore the damn errors

--- frontend/components/Navigation/Navigation.tsx
+++ frontend/components/Navigation/Navigation.tsx
@@ -1,12 +1,18 @@
 import { PathsEntity } from "../../types";
 
+type params = {
+  news: string
+}
+
 export const Navigation = (paramsObj: PathsEntity) => {
   // @ts-ignore
-  const params = paramsObj.params.params;
+  const params: params = paramsObj.params.params;
 
   return (
+    // @ts-ignore
     <a href={`news/${params.news}`} key={params.news}>
       {params.news}
+      {/* @ts-ignore */}
     </a>
   );
 };
